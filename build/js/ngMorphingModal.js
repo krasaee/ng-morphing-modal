6f99c881af5befff57b18458a4608fdf684d07e7
Kasra Rasaee <krasaee@straata.com>
2015-08-31 18:00:09 -0400

    refactored code to work more inline with angular versus use of jquery

--- build/js/ngMorphingModal.js
+++ build/js/ngMorphingModal.js
@@ -1,181 +1,160 @@
-(function (window, document) {
+var ngMorphingModal = angular.module('ngMorphingModal', []);
 
-    MorphingModal = function (options) {
+ngMorphingModal.directive('ngMorphingModal', function () {
+  return {
+    replace: true,
+    restrict: 'AE',
+    scope: {
+      contentSelector: '@',
+      selectorId: "@?",
+      buttonOpenId: "@",
+      buttonCloseId: "@",
+      afterOpenFn: '&?',
+      afterCloseFn: '&?'
+    },
+    link: function ($scope, element, attrs) {
+
+      //var randLetter = String.fromCharCode(65 + Math.floor(Math.random() * 26));
+      //var uniqid = randLetter + Date.now();
+
+      var clickCallback = function (event) {
+
+        // if its the close button
+        if (event.target.hasAttribute('close-id')) {
+          var btn = element.find('[close-id="' + $scope.buttonCloseId + '"]');
+          $scope.closeModal(btn);
+
+          if (angular.isDefined(attrs.afterCloseFn)) {
+            $scope.afterCloseFn(btn);
+          }
+
+          return;
+        }
+
+        // if its the open button
+        if (event.target.hasAttribute('open-id')) {
+          var btn = element.find('[open-id="' + $scope.buttonOpenId + '"]');
+          $scope.openModal(btn);
+
+          if (angular.isDefined(attrs.afterOpenFn)) {
+            $scope.afterOpenFn(btn);
+          }
+
+          return;
+        }
+
+        console.log('Invalid callback, no open-id and or close-id defined for this morph modal', element, $scope);
+      };
+
+      element.bind('click', clickCallback);
+    },
+    templateUrl: function (tElement, tAttrs) {
+      if (tAttrs.templateUrl != null) {
+        return tAttrs.templateUrl;
+      }
+
+      return 'ng-morphing-modal.html';
+    },
+    controller: function ($scope) {
+
+      /**
+       * Handle the opening of the modal
+       *
+       * @param button
+       */
+      $scope.openModal = function (button) {
+        var scaleValue = $scope.retrieveScale(button.next('.cd-modal-bg'));
+
+        button.addClass('to-circle');
+        button.next('.cd-modal-bg').addClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function () {
+          $scope.animateLayer(button.next('.cd-modal-bg'), scaleValue, true);
+        });
+
+        //if browser doesn't support transitions...
+        if (button.parents('.no-csstransitions').length > 0) {
+          $scope.animateLayer(button.next('.cd-modal-bg'), scaleValue, true);
+        }
+      }
+
+      /**
+       * Handles the closing of the modal
+       *
+       * @param contentSelector
+       */
+      $scope.closeModal = function (button, contentSelector) {
+        // TODO should be specific to the selector (of sorts...) - although this probably does not really matter
+
+        var section = $('.cd-section.modal-is-visible');
+        section.removeClass('modal-is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',
+          function () {
+            $scope.animateLayer(section.find('.cd-modal-bg'), 1, false);
+
+
+          }
+        );
+
+        //if browser doesn't support transitions...
+        if (section.parents('.no-csstransitions').length > 0) {
+          $scope.animateLayer(section.find('.cd-modal-bg'), 1, false);
+        }
+      }
+
+      $scope.retrieveScale = function (btn) {
+        var that = this;
+        if (btn.offset()) {
+          var btnRadius = btn.width() / 2,
+            left = btn.offset().left + btnRadius,
+            top = btn.offset().top + btnRadius - $(window).scrollTop(),
+            scale = that.scaleValue(top, left, btnRadius, $(window).height(), $(window).width());
+
+          btn.css('position', 'fixed').velocity({
+            top: top - btnRadius,
+            left: left - btnRadius,
+            translateX: 0
+          }, 0);
+        } else {
+          scale = 1;
+        }
+        return scale;
+
+      };
+
+      $scope.scaleValue = function (topValue, leftValue, radiusValue, windowW, windowH) {
+        var maxDistHor = ( leftValue > windowW / 2) ? leftValue : (windowW - leftValue),
+          maxDistVert = ( topValue > windowH / 2) ? topValue : (windowH - topValue);
+        return Math.ceil(Math.sqrt(Math.pow(maxDistHor, 2) + Math.pow(maxDistVert, 2)) / radiusValue);
+      };
+
+      $scope.animateLayer = function (layer, scaleVal, bool) {
 
         var that = this;
 
-        that.selectorId = '[data-type="modal-trigger"]';//options.selector;
-        that.selectorObj = $('[data-type="modal-trigger"]');
-        that.contentSelector = '.cd-modal';
-        that.contentSelectorObj = $('.cd-modal');
+        layer.velocity({scale: scaleVal}, 400, function () {
+          $('body').toggleClass('overflow-hidden', bool);
+          (bool)
+            ? layer.parents('.cd-section').addClass('modal-is-visible').end().off('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend')
+            : layer.removeClass('is-visible').removeAttr('style').siblings($scope.selectorId).removeClass('to-circle');
+        });
+      }
 
+      $scope.updateLayer = function () {
 
+        var that = this;
 
-        // DEFAULT OPTIONS
-        that.options = {
-            selectorId: false,
-            selectorObj: false,
-            contentSelector: false,
-            onAfterOpen: null,
-            onAfterClose: null
+        var layer = $('.cd-section.modal-is-visible').find('.cd-modal-bg'),
+          layerRadius = layer.width() / 2,
+          layerTop = layer.siblings('.btn').offset().top + layerRadius - $(window).scrollTop(),
+          layerLeft = layer.siblings('.btn').offset().left + layerRadius,
+          scale = that.scaleValue(layerTop, layerLeft, layerRadius, $(window).height(), $(window).width());
 
-        };
+        layer.velocity({
+          top: layerTop - layerRadius,
+          left: layerLeft - layerRadius,
+          scale: scale
+        }, 0);
+      }
 
-        // OVERWRITE DEFAULT OPTIONS
-        for (i in options) that.options[i] = options[i];
-        console.log(that.contentSelector);
-        // INIT THE WHOLE DAMN THING!!!
-        that.init();
-
-    };
-    MorphingModal.prototype = {
-
-        obj: {},
-        containerObj: {},
-        init: function () {
-            var that = this;
-
-            that.corners = this.options.corners;
-
-
-            jQuery(document).ready(function ($) {
-                that.create();
-            });
-
-        },
-        create: function () {
-            var that = this;
-
-            //trigger the animation - open modal window
-            that.selectorObj.on('click', function () {
-                var actionBtn = $(this),
-                    scaleValue = that.retrieveScale(actionBtn.next('.cd-modal-bg')); //c
-
-                actionBtn.addClass('to-circle');
-                actionBtn.next('.cd-modal-bg').addClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function () {
-                    that.animateLayer(actionBtn.next('.cd-modal-bg'), scaleValue, true);
-                });
-
-                //if browser doesn't support transitions...
-                if (actionBtn.parents('.no-csstransitions').length > 0) that.animateLayer(actionBtn.next('.cd-modal-bg'), scaleValue, true);
-            });
-
-            //trigger the animation - close modal window
-            $('.cd-section .cd-modal-close').on('click', function () {
-                that.closeModal();
-            });
-            $(document).keyup(function (event) {
-                if (event.which == '27') that.closeModal();
-            });
-
-            $(window).on('resize', function () {
-                //on window resize - update cover layer dimention and position
-                if ($('.cd-section.modal-is-visible').length > 0) window.requestAnimationFrame(that.updateLayer);
-            });
-        },
-        retrieveScale: function (btn) {
-
-            var that = this;
-if(btn.offset()){
-
-
-            var btnRadius = btn.width() / 2,
-                left = btn.offset().left + btnRadius,
-                top = btn.offset().top + btnRadius - $(window).scrollTop(),
-                scale = that.scaleValue(top, left, btnRadius, $(window).height(), $(window).width());
-
-            btn.css('position', 'fixed').velocity({
-                top: top - btnRadius,
-                left: left - btnRadius,
-                translateX: 0
-            }, 0);
-}else{
-    scale=1;
-}
-            return scale;
-
-        },
-        scaleValue: function (topValue, leftValue, radiusValue, windowW, windowH) {
-
-            var that = this;
-
-            var maxDistHor = ( leftValue > windowW / 2) ? leftValue : (windowW - leftValue),
-                maxDistVert = ( topValue > windowH / 2) ? topValue : (windowH - topValue);
-            return Math.ceil(Math.sqrt(Math.pow(maxDistHor, 2) + Math.pow(maxDistVert, 2)) / radiusValue);
-        },
-        animateLayer: function (layer, scaleVal, bool) {
-
-            var that = this;
-
-            layer.velocity({ scale: scaleVal }, 400, function () {
-                $('body').toggleClass('overflow-hidden', bool);
-                (bool)
-                    ? layer.parents('.cd-section').addClass('modal-is-visible').end().off('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend')
-                    : layer.removeClass('is-visible').removeAttr('style').siblings(that.selectorId).removeClass('to-circle');
-            });
-        },
-        updateLayer: function () {
-
-            var that = this;
-
-            var layer = $('.cd-section.modal-is-visible').find('.cd-modal-bg'),
-                layerRadius = layer.width() / 2,
-                layerTop = layer.siblings('.btn').offset().top + layerRadius - $(window).scrollTop(),
-                layerLeft = layer.siblings('.btn').offset().left + layerRadius,
-                scale = that.scaleValue(layerTop, layerLeft, layerRadius, $(window).height(), $(window).width());
-
-            layer.velocity({
-                top: layerTop - layerRadius,
-                left: layerLeft - layerRadius,
-                scale: scale
-            }, 0);
-        },
-        closeModal: function () {
-
-            var that = this;
-
-            var section = $('.cd-section.modal-is-visible');
-            section.removeClass('modal-is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function () {
-                that.animateLayer(section.find('.cd-modal-bg'), 1, false);
-            });
-            //if browser doesn't support transitions...
-            if (section.parents('.no-csstransitions').length > 0) animateLayer(section.find('.cd-modal-bg'), 1, false);
-        },
-
-        destroy: function () {
-
-        }
-    }
-})(window, document);
-var myApp = angular.module('myApp', []).directive(
-    'ngMorphingModal', function() {
-        var randLetter = String.fromCharCode(65 + Math.floor(Math.random() * 26));
-        var uniqid = randLetter + Date.now();
-
-        return {
-            replace: true,
-            restrict: 'AE',
-            scope: {
-                contentSelector: '@'
-            },
-            link: function (scope, element, attributes) {
-
-            var MorphingModalObj = new MorphingModal({
-                selectorId: '[data-type="modal-trigger"]',
-                selectorObj: $('[data-type="modal-trigger"]'),
-                contentSelector: scope.contentSelector,
-                onAfterClose: null,
-                onAfterOpen: function(){}
-            });
-
-        },
-            templateUrl: '../js/td-modal.html'
 
     }
-    });
-
-
-myApp.controller("MyController", ["$scope", function($scope) {
-    $scope.name = "World";
-}]);
-
+  }
+});
--- js/ngMorphingModal.js
+++ js/ngMorphingModal.js
@@ -1,32 +1,160 @@
-var myApp = angular.module('myApp', []).directive(
-    'ngMorphingModal', function() {
-        var randLetter = String.fromCharCode(65 + Math.floor(Math.random() * 26));
-        var uniqid = randLetter + Date.now();
+var ngMorphingModal = angular.module('ngMorphingModal', []);
 
-        return {
-            replace: true,
-            restrict: 'AE',
-            scope: {
-                contentSelector: '@'
-            },
-            link: function (scope, element, attributes) {
+ngMorphingModal.directive('ngMorphingModal', function () {
+  return {
+    replace: true,
+    restrict: 'AE',
+    scope: {
+      contentSelector: '@',
+      selectorId: "@?",
+      buttonOpenId: "@",
+      buttonCloseId: "@",
+      afterOpenFn: '&?',
+      afterCloseFn: '&?'
+    },
+    link: function ($scope, element, attrs) {
 
-            var MorphingModalObj = new MorphingModal({
-                selectorId: '[data-type="modal-trigger"]',
-                selectorObj: $('[data-type="modal-trigger"]'),
-                contentSelector: scope.contentSelector,
-                onAfterClose: null,
-                onAfterOpen: function(){}
-            });
+      //var randLetter = String.fromCharCode(65 + Math.floor(Math.random() * 26));
+      //var uniqid = randLetter + Date.now();
 
-        },
-            templateUrl: '../js/td-modal.html'
+      var clickCallback = function (event) {
+
+        // if its the close button
+        if (event.target.hasAttribute('close-id')) {
+          var btn = element.find('[close-id="' + $scope.buttonCloseId + '"]');
+          $scope.closeModal(btn);
+
+          if (angular.isDefined(attrs.afterCloseFn)) {
+            $scope.afterCloseFn(btn);
+          }
+
+          return;
+        }
+
+        // if its the open button
+        if (event.target.hasAttribute('open-id')) {
+          var btn = element.find('[open-id="' + $scope.buttonOpenId + '"]');
+          $scope.openModal(btn);
+
+          if (angular.isDefined(attrs.afterOpenFn)) {
+            $scope.afterOpenFn(btn);
+          }
+
+          return;
+        }
+
+        console.log('Invalid callback, no open-id and or close-id defined for this morph modal', element, $scope);
+      };
+
+      element.bind('click', clickCallback);
+    },
+    templateUrl: function (tElement, tAttrs) {
+      if (tAttrs.templateUrl != null) {
+        return tAttrs.templateUrl;
+      }
+
+      return 'ng-morphing-modal.html';
+    },
+    controller: function ($scope) {
+
+      /**
+       * Handle the opening of the modal
+       *
+       * @param button
+       */
+      $scope.openModal = function (button) {
+        var scaleValue = $scope.retrieveScale(button.next('.cd-modal-bg'));
+
+        button.addClass('to-circle');
+        button.next('.cd-modal-bg').addClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function () {
+          $scope.animateLayer(button.next('.cd-modal-bg'), scaleValue, true);
+        });
+
+        //if browser doesn't support transitions...
+        if (button.parents('.no-csstransitions').length > 0) {
+          $scope.animateLayer(button.next('.cd-modal-bg'), scaleValue, true);
+        }
+      }
+
+      /**
+       * Handles the closing of the modal
+       *
+       * @param contentSelector
+       */
+      $scope.closeModal = function (button, contentSelector) {
+        // TODO should be specific to the selector (of sorts...) - although this probably does not really matter
+
+        var section = $('.cd-section.modal-is-visible');
+        section.removeClass('modal-is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',
+          function () {
+            $scope.animateLayer(section.find('.cd-modal-bg'), 1, false);
+
+
+          }
+        );
+
+        //if browser doesn't support transitions...
+        if (section.parents('.no-csstransitions').length > 0) {
+          $scope.animateLayer(section.find('.cd-modal-bg'), 1, false);
+        }
+      }
+
+      $scope.retrieveScale = function (btn) {
+        var that = this;
+        if (btn.offset()) {
+          var btnRadius = btn.width() / 2,
+            left = btn.offset().left + btnRadius,
+            top = btn.offset().top + btnRadius - $(window).scrollTop(),
+            scale = that.scaleValue(top, left, btnRadius, $(window).height(), $(window).width());
+
+          btn.css('position', 'fixed').velocity({
+            top: top - btnRadius,
+            left: left - btnRadius,
+            translateX: 0
+          }, 0);
+        } else {
+          scale = 1;
+        }
+        return scale;
+
+      };
+
+      $scope.scaleValue = function (topValue, leftValue, radiusValue, windowW, windowH) {
+        var maxDistHor = ( leftValue > windowW / 2) ? leftValue : (windowW - leftValue),
+          maxDistVert = ( topValue > windowH / 2) ? topValue : (windowH - topValue);
+        return Math.ceil(Math.sqrt(Math.pow(maxDistHor, 2) + Math.pow(maxDistVert, 2)) / radiusValue);
+      };
+
+      $scope.animateLayer = function (layer, scaleVal, bool) {
+
+        var that = this;
+
+        layer.velocity({scale: scaleVal}, 400, function () {
+          $('body').toggleClass('overflow-hidden', bool);
+          (bool)
+            ? layer.parents('.cd-section').addClass('modal-is-visible').end().off('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend')
+            : layer.removeClass('is-visible').removeAttr('style').siblings($scope.selectorId).removeClass('to-circle');
+        });
+      }
+
+      $scope.updateLayer = function () {
+
+        var that = this;
+
+        var layer = $('.cd-section.modal-is-visible').find('.cd-modal-bg'),
+          layerRadius = layer.width() / 2,
+          layerTop = layer.siblings('.btn').offset().top + layerRadius - $(window).scrollTop(),
+          layerLeft = layer.siblings('.btn').offset().left + layerRadius,
+          scale = that.scaleValue(layerTop, layerLeft, layerRadius, $(window).height(), $(window).width());
+
+        layer.velocity({
+          top: layerTop - layerRadius,
+          left: layerLeft - layerRadius,
+          scale: scale
+        }, 0);
+      }
+
 
     }
-    });
-
-
-myApp.controller("MyController", ["$scope", function($scope) {
-    $scope.name = "World";
-}]);
-
+  }
+});
